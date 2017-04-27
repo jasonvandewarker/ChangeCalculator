b55824685f5d28334cab054eef91c73ac4fec029
jasonvandewarker <jasonvandewarker@gmail.com>
2017-04-26 17:02:16 -0700

    CSS added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-  $('button').click(function() {
+  $('button').click(function() {  //click event that starts the calculations
 
+/* this block grabs the numbers inputed into the form boxes, stores them
+in variables. Then it creates a variable after subtracting them from each other. */
     var product = $('#productcost').val();
     var customer = $('#custamount').val();
     var sum = (customer - product).toFixed(2);
 
+/*   */
     var dollars = Math.floor(sum).toFixed(0);
     var remainder = ((sum - dollars) * 100).toFixed(0);
 
@@ -16,11 +19,11 @@ $(document).ready(function() {
     remainder %= 5;
     var pennies = remainder;
 
-    $('#dollars').append("Give the customer " + dollars + " dollars.");
-    $('#quarters').append("Give the customer " + quarters + " quarters.");
-    $('#dimes').append("Give the customer " + dimes + " dimes.");
-    $('#nickels').append("Give the customer " + nickels + " nickels.");
-    $('#pennies').append("Give the customer " + pennies + " pennies.");
+    $('#dollars').append("Give the customer: " + dollars + " dollars.");
+    $('#quarters').append(quarters + " quarters.");
+    $('#dimes').append(dimes + " dimes.");
+    $('#nickels').append(nickels + " nickels.");
+    $('#pennies').append(pennies + " pennies.");
 
   }); //end of click
 }); //end of .ready
