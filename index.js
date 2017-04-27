9d05eb3a2ef6807a85dc31a665b3b5b6e584350d
jasonvandewarker <jasonvandewarker@gmail.com>
2017-04-27 13:06:36 -0700

    Finished

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-  $('button').click(function() {  //click event that starts the calculations
+  $('button').click(function() { //click event that starts the calculations
 
-/* this block grabs the numbers inputed into the form boxes, stores them
-in variables. Then it creates a variable after subtracting them from each other. */
+    /* this block grabs the numbers inputed into the form boxes, stores them
+    in variables. Then it creates a variable after subtracting them from each other. */
     var product = $('#productcost').val();
     var customer = $('#custamount').val();
     var sum = (customer - product).toFixed(2);
 
-/*   */
+    /* This block of code represents the math part. The dollars are taken
+    off of the top, and then the remainder is multiplied by 100 to make whole
+    numbers. The permutations of change are taken out, and what is left each time
+    is stored in the "remainder" variable.  */
     var dollars = Math.floor(sum).toFixed(0);
     var remainder = ((sum - dollars) * 100).toFixed(0);
-
     var quarters = Math.floor(remainder / 25).toFixed(0);
     remainder %= 25;
     var dimes = Math.floor(remainder / 10).toFixed(0);
@@ -19,6 +21,7 @@ in variables. Then it creates a variable after subtracting them from each other.
     remainder %= 5;
     var pennies = remainder;
 
+    //The block of code posts the amount of change to the DOM
     $('#dollars').append("Give the customer: " + dollars + " dollars.");
     $('#quarters').append(quarters + " quarters.");
     $('#dimes').append(dimes + " dimes.");
